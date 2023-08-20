3e3f58da269369837b8d1dc48473946451d2b274
Batista <francinaldofilho05@gmail.com>
2023-08-20 16:17:01 -0300

    Atualização da Classe ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,8 @@
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // to do: Importar a classe Scanner
+        // Exibir as mensagens para o usuário.
+        // Obter informações através do Scanner
+        // Exibir a mensagem da conta criada.
     }
 }
